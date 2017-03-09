2dd71baeb3b3133ddfd06db7b831bf22bd994b05
XylugaH <XylugaH3k@gmail.com>
2017-03-09 12:57:29 +0200

    Confegure project

--- src/main/java/com/xylugah/controller/LoginController.java
+++ src/main/java/com/xylugah/controller/LoginController.java
@@ -1,38 +1,23 @@
 package com.xylugah.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
-public class HomeController {
+import com.xylugah.entity.User;
 
-	private static final Logger logger = Logger.getLogger(HomeController.class);
+@RestController
+public class LoginController {
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	private static final Logger logger = Logger.getLogger(LoginController.class);
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public User home(@RequestParam(value = "name", defaultValue = "World") String name) {
 		logger.info("Welcome home! The client locale is {}.");
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
-		return "home";
+		return new User("XylugaH", 1, "123");
 	}
 
 }
