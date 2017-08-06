47db7528d31842b0365404b60849982bffd15b88
XylugaH <XylugaH3k@gmail.com>
2017-08-06 20:03:37 +0300

    Add controller

---
+++ src/main/java/com/xylugah/controller/ExitController.java
@@ -0,0 +1,13 @@
+package com.xylugah.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/exit")
+public class ExitController {
+
+    public void exit(){
+
+    }
+}
